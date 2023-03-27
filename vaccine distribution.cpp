c0769d16c77ef6aab5b74edafb99444ddff9f14d
Rupam Tiwary <Rupamtiwary258@gmail.com>
2023-03-27 22:53:18 +0530

    intial

---
+++ vaccine distribution.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;cin>>t;
+	while(t--){
+	    float n,d,age,cr=0,cnr=0;
+	    cin>>n>>d;
+	    for(int i=0;i<n;i++){
+	        cin>>age;
+	        if(age>=80 || age<=9) cr++;
+	        else cnr++;
+	    }
+	    int d1=ceil(cr/d);
+	    int d2=ceil(cnr/d);
+	    cout<< d1+d2 <<endl;
+	    
+	}
+	return 0;
+}
